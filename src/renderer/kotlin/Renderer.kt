0bc753b79a7a3848031e7e4feab4f4a45116a26f
Tom Warner <tomwamt@gmail.com>
2019-12-27 13:02:39 -0700

    Save data on close

--- src/renderer/kotlin/Renderer.kt
+++ src/renderer/kotlin/Renderer.kt
@@ -1,9 +1,11 @@
 import components.WatchlistComponent
 import services.DataRepository
 import services.WatchlistService
+import kotlin.browser.window
 
 fun main() {
     val data = DataRepository()
     val service = WatchlistService(data)
     kui.mountComponent("app", WatchlistComponent(service))
+    window.addEventListener("beforeunload", { service.saveToDisk() })
 }
